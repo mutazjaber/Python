0beb4349fd58abbe0ee7aa0a8972d1f503744f29
mutaz jaber <mutaz-mhj@hotmail.com>
2023-11-07 23:16:21 +0200

    Hello World

---
+++ _python/python_fundamentals/hello_world.py
@@ -0,0 +1,22 @@
+# name = "Mutaz jaber"
+# print(f"Hello {name}")
+
+
+
+# 1. TASK: print "Hello World"
+# print( "Hello World")
+# 2. print "Hello Noelle!" with the name in a variable
+# name = "mutaz"
+# print(f"hello, {name},!")	# with a comma
+# print(f"hello, + {name} +")	# with a +
+# 3. print "Hello 42!" with the number in a variable
+# name = 42
+# print(f"Hello", {name} , "!" )	# with a comma
+# print(f"Hello" + {name} +"!"  )	# with a +	-- this one should give us an error!
+# # 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+# print( "i love to eat {} and {} ".format(fave_food1,fave_food2) ) # with .format()
+# print( "i love to eat %s and %s"%(fave_food1,fave_food2) ) # with an f string
+# print(f"I love to eat {fave_food1} and {fave_food2}.")
+
