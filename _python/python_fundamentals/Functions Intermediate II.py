ffcbe0fb02b4b705ec07fe80f8cf94ba7610ffd2
mutaz jaber <mutaz-mhj@hotmail.com>
2023-11-12 13:16:57 +0200

    flask_1

--- _python/python_fundamentals/Functions Intermediate II.py
+++ _python/python_fundamentals/Functions Intermediate II.py
@@ -11,7 +11,7 @@
 
 # x[1][0]=15
 # print(x)
-# # Output: [[5, 2, 3], [15, 8, 9]]
+# Output: [[5, 2, 3], [15, 8, 9]]
 # students[0]["last_name"]="Bryant"
 # print(students)
 # sports_directory['soccer'][0]='Adnres'
@@ -19,23 +19,19 @@
 # z[0]['y']= 30
 # print(z)
 
-students = [
-         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-         {'first_name' : 'John', 'last_name' : 'Rosales'},
-         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-         {'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
-# iterateDictionary(students) 
-# # should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# # bonus to get them to appear exactly as below!)
-# first_name - Michael, last_name - Jordan
-# first_name - John, last_name - Rosales
-# first_name - Mark, last_name - Guillen
-# first_name - KB, last_name - Tonel
+# def iterateDictionary(some_list):
+#     for student in (some_list):
+#         print('first_name - ' + student['first_name'] + ', last_name - ' + student['last_name'])
 
-def iterateDictionary(some_list):
-    for i in range (0,len(some_list)):
-        xy=students.get("first_name" ,{"first_name"}, "last_name",{"last_name"})
-        print(xy)
+# students = [
+#     {'first_name': 'Michael', 'last_name': 'Jordan'},
+#     {'first_name': 'John', 'last_name': 'Rosales'},
+#     {'first_name': 'Mark', 'last_name': 'Guillen'},
+#     {'first_name': 'KB', 'last_name': 'Tonel'}
+# ]
+
+# iterateDictionary(students)
+
+
+def iterateDictionary2(key_name, some_list):
     
-iterateDictionary(students)
---
+++ flask/flask_fundemental/Understanding_Routing/New Text Document.py
@@ -0,0 +1,42 @@
+from flask import Flask
+import string
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/dojo')          # The "@" decorator associates this route with the function immediately following
+def dojo():
+    return 'Dojo!' 
+@app.route('/<name>')
+def hi(name):
+    return 'HI' + name.title() + '!'
+@app.route('/repeat/<num>/<str>')
+def repeat(num,str):
+    strs = ''
+    for i in range (int(num)):
+        strs +=f'\n{str.title()}'
+    return strs
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run
+
+
+
+    from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+@app.route('/<name>')
+def dojo(name):
+    return name.title() + '!'
+@app.route('/say/<name>')
+def say(name):
+    return f'Hi {name} !'
+@app.route('/repeat/<num>/<str>')
+def repeat(num, str):
+    strs = ''
+    for i in range (int(num)):
+        strs+=f'\n{str.title()}'
+    return strs
+if __name__=="__main__":
+    app.run(debug=True)
