fefc628ef8bb902e523bfe0ffda27554e6181e5a
mutaz jaber <mutaz-mhj@hotmail.com>
2023-11-20 08:10:14 +0200

    adding

--- _python/python_fundamentals/Functions Intermediate II.py
+++ _python/python_fundamentals/Functions Intermediate II.py
@@ -32,6 +32,29 @@
 
 # iterateDictionary(students)
 
+# def iterateDictionary2(key_name, some_list):
+#     for student in (some_list):
+#         print(student[key_name])
+            
+# students = [
+#     {'first_name': 'Michael', 'last_name': 'Jordan'},
+#     {'first_name': 'John', 'last_name': 'Rosales'},
+#     {'first_name': 'Mark', 'last_name': 'Guillen'},
+#     {'first_name': 'KB', 'last_name': 'Tonel'}
+# ]
+# iterateDictionary2("last_name",students)
+ 
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
 
-def iterateDictionary2(key_name, some_list):
-    
+def printInfo(some_dict):
+    for key in dojo :
+        print (len(dojo[key]),key)
+        for i in dojo[key]:
+            print(i)
+        print()
+        
+
+printInfo(dojo)
